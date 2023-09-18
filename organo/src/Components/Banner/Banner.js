f04c9d71b0f689ac22936b0151ded7d7df139844
Fabio Ishizu <fabio.masao@tecdet.com.br>
2023-09-18 17:44:48 -0300

    salvando progresso

---
+++ organo/src/Components/Banner/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Banner(){
+    return(
+        <>
+            <img src="/Images/banner.png" alt="Imagem do banner inicial"/>
+        </>
+    )
+}
